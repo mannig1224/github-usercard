25a6632506031e004c1f60b4a9a6dd5e7c971765
ManniG24 <egatica51@gmail.com>
2021-09-02 16:13:03 -0600

    finished project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,12 @@
+import axios from 'axios';
+console.log(axios);
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/mannig1224')
+  
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -16,7 +19,9 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
+.then(result => {
+  document.querySelector('.cards').appendChild(cardMaker(result.data));
+})
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -24,12 +29,25 @@
     bottom of the page. Get at least 5 different Github usernames and add them as
     Individual strings to the friendsArray below.
 
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
+    Using that array, 
+    iterate over it, 
+    requesting data for each user, 
+    creating a new card for each
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
 
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+const usernames = followersArray.map(username =>  "https://api.github.com/users/" + username );
+console.log(usernames);
+
+const promiseArray = usernames.map(getRequest => axios.get(getRequest));
+console.log(promiseArray);
+
+Promise.all(promiseArray)
+  .then(responses => {
+    responses.forEach(response => document.querySelector('.cards').appendChild(cardMaker(response.data)))
+});
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -49,12 +67,51 @@ const followersArray = [];
       </div>
     </div>
 */
+function cardMaker({avatar_url, name, login, location, html_url, followers, following}) {
+  
+  // creating the elements
+  const cardContainer = document.createElement('div');
+  const cardImage = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const cardName = document.createElement('h3');
+  const cardUsername = document.createElement('p');
+  const cardLocation = document.createElement('p');
+  const cardProfile = document.createElement('p');
+  const cardLink = document.createElement('a');
+  const cardFollowers = document.createElement('p');
+  const cardFollowing = document.createElement('p');
+  const cardBio = document.createElement('p');
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+
+  // appending elements
+  cardContainer.appendChild(cardImage);
+  cardContainer.appendChild(cardInfo);
+  cardInfo.appendChild(cardName);
+  cardInfo.appendChild(cardUsername);
+  cardInfo.appendChild(cardLocation);
+  cardInfo.appendChild(cardProfile);
+  cardInfo.appendChild(cardFollowers);
+  cardInfo.appendChild(cardFollowing);
+  cardInfo.appendChild(cardBio);
+  cardProfile.appendChild(cardLink);
+
+  //adding classNames to elements
+  cardContainer.classList.add('card');
+  cardInfo.classList.add('card-info');
+  cardName.classList.add('name');
+  cardUsername.classList.add('username');
+
+  //adding content
+  cardImage.src = avatar_url;
+  cardName.textContent = name;
+  cardUsername.textContent = login;
+  cardLocation.textContent = location;
+  cardLink.textContent = html_url;
+  cardFollowers.textContent = `Followers: ${followers}`;
+  cardFollowing.textContent = `Following: ${following}`;
+
+
+  return cardContainer;
+}
+
+
